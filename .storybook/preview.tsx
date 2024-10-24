ea18f733fb829343016ddcb312fed60f8f5e9f2c
devpoi <smboy86@naver.com>
2024-10-24 15:30:26 +0900

    fail - 스토리북 설치 실패

---
+++ .storybook/preview.tsx
@@ -0,0 +1,52 @@
+// import type { Preview } from "@storybook/react";
+
+// const preview: Preview = {
+//   parameters: {},
+//   decorators: [],
+// };
+
+// export default preview;
+
+export const parameters = {
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
+
+// import { View, Appearance } from 'react-native';
+// import { withBackgrounds } from '@storybook/addon-ondevice-backgrounds';
+// import type { Preview } from '@storybook/react';
+
+// const preview: Preview = {
+//   decorators: [
+//     (Story) => (
+//       <View style={{ padding: 8, flex: 1 }}>
+//         <Story />
+//       </View>
+//     ),
+//     withBackgrounds,
+//   ],
+//   parameters: {
+//     actions: { argTypesRegex: '^on[A-Z].*' },
+//     controls: {
+//       matchers: {
+//         color: /(background|color)$/i,
+//         date: /Date$/,
+//       },
+//     },
+//     my_param: 'anything',
+//     backgrounds: {
+//       default: Appearance.getColorScheme() === 'dark' ? 'dark' : 'plain',
+//       values: [
+//         { name: 'plain', value: 'white' },
+//         { name: 'dark', value: '#333' },
+//         { name: 'app', value: '#eeeeee' },
+//       ],
+//     },
+//   },
+// };
+
+// export default preview;
