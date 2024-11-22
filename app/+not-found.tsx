0d55ff7c878ecabff29bceedd03c79a05e8b2abf
devpoi <smboy86@naver.com>
2024-11-22 11:21:20 +0900

    mod - 링크

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -5,12 +5,12 @@ import { Text } from '~/components/ui/text';
 export default function NotFoundScreen() {
   return (
     <>
-      <Stack.Screen options={{ title: 'Not Page', headerBackTitleVisible: false }} />
+      <Stack.Screen options={{ title: 'Not Page', headerBackVisible: false }} />
       <View>
         <Text>해당 화면이 존재하지 않습니다!</Text>
 
-        <Link href='/'>
-          <Text>메인으로 돌아갑니다.</Text>
+        <Link href='/(main)/home'>
+          <Text>메인으로 돌아갑니다. </Text>
         </Link>
       </View>
     </>
