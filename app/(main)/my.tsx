071933c1dc2e17e0b5ae7f54e0396c42622d4484
devpoi <smboy86@naver.com>
2024-12-09 13:31:59 +0900

    dev - 회원탈퇴 기능 (supabase edge function)

--- app/(main)/my.tsx
+++ app/(main)/my.tsx
@@ -6,44 +6,29 @@ import { Container } from '~/components/layout/container';
 import { Header } from '~/components/layout/header';
 import { Button } from '~/components/ui/button';
 import { Text } from '~/components/ui/text';
-import { supabase } from '~/utils/supabase';
 import { Alert } from 'react-native';
 
 export default function My() {
   const router = useRouter();
   const { isLogin } = useAuth();
 
-  const handleLogout = async () => {
-    Alert.alert('미팅을 잡자', '로그아웃 하시겠습니까?', [
-      { text: '취소', style: 'cancel' },
-      {
-        text: '확인',
-        onPress: async () => {
-          const { error } = await supabase.auth.signOut();
-          if (error) {
-            console.error(error);
-          }
-
-          router.replace('/(main)/home');
-        },
-      },
-    ]);
-  };
-
   return (
     <Container main className='items-center justify-center'>
       <Stack.Screen options={{ title: 'Sign up' }} />
       <Header type='default' />
       <Wrap type='default' full>
-        <Link
-          href={{
-            pathname: '/home',
-          }}
-          asChild>
-          <Button variant='outline' className='mb-2 border border-brand'>
-            <Text>앱정보</Text>
-          </Button>
-        </Link>
+        <Button
+          variant='outline'
+          className='mb-2 border border-brand'
+          onPress={() => {
+            if (!isLogin) {
+              Alert.alert('미팅을 잡자', '로그인이 필요합니다.');
+              return;
+            }
+            router.push('/detail/myInfo');
+          }}>
+          <Text>내정보</Text>
+        </Button>
         <Link
           href={{
             pathname: '/detail/setting/resetPassword',
@@ -62,11 +47,7 @@ export default function My() {
             <Text>스토어 평점주기</Text>
           </Button>
         </Link>
-        {isLogin ? (
-          <Button variant='outline' className='mb-2 border-[#E5E5EC] bg-[#E5E5EC]' onPress={handleLogout}>
-            <Text className='text-[#505050]'>로그아웃</Text>
-          </Button>
-        ) : (
+        {!isLogin && (
           <Link
             href={{
               pathname: '/auth/login',
---
+++ app/detail/myInfo.tsx
@@ -0,0 +1,87 @@
+import { unlink } from '@react-native-kakao/user';
+import { useRouter } from 'expo-router';
+import { useState } from 'react';
+import { Alert, View } from 'react-native';
+import { Wrap } from '~/components/layout/\bwrap';
+import { Container } from '~/components/layout/container';
+import { Button } from '~/components/ui/button';
+import { Input } from '~/components/ui/input';
+import { Text } from '~/components/ui/text';
+import { useAuth } from '~/providers/AuthProvider';
+import { supabase } from '~/utils/supabase';
+
+export default function Screen() {
+  const router = useRouter();
+  const { session } = useAuth();
+
+  const handleLogout = async () => {
+    Alert.alert('미팅을 잡자', '로그아웃 하시겠습니까?', [
+      { text: '취소', style: 'cancel' },
+      {
+        text: '확인',
+        onPress: async () => {
+          const { error } = await supabase.auth.signOut();
+          if (error) {
+            console.error(error);
+          }
+
+          router.replace('/(main)/home');
+        },
+      },
+    ]);
+  };
+
+  const deleteUser = () => {
+    Alert.alert('미팅을 잡자', '회원탈퇴 하시겠습니까?', [
+      { text: '취소', style: 'cancel' },
+      {
+        text: '확인',
+        onPress: async () => {
+          try {
+            // 0) (필요시) data table 삭제
+            // 1) supabase auth 유저 삭제
+            // endpoint - https://dhlfezdmcnodzwreqzrj.supabase.co/functions/v1/delete-user
+            await supabase.functions.invoke('delete-user');
+            // 2) supabase 로그아웃
+            await supabase.auth.signOut();
+            // 3) social 언링크
+            if (session?.user.app_metadata.provider === 'kakao') {
+              unlink();
+            }
+            Alert.alert('회원탈퇴가 정상적으로 처리 되었습니다.\n이용해주셔서 감사합니다.');
+          } catch (error) {
+            console.log('errrr  ', error);
+          } finally {
+            router.replace('/(main)/home');
+          }
+        },
+      },
+    ]);
+  };
+
+  // useEffect(() => {
+  // console.log('ffff  ', session?.user);
+  // console.log('ffff  ', session?.user.app_metadata.provider);
+  // });
+
+  return (
+    <>
+      {/* <Stack.Screen options={{ title: '스크린 제목' }} /> */}
+      <Container className='items-center justify-center'>
+        <Wrap type='default' full>
+          <View className=''>
+            <Input editable={false} value={session?.user.email} className='mb-[12px]' />
+            <View>
+              <Button variant='outline' className='mb-2 border-[#E5E5EC] bg-[#E5E5EC]' onPress={handleLogout}>
+                <Text className='text-[#505050]'>로그아웃</Text>
+              </Button>
+              <Button variant='outline' className='mb-2 border-[#E5E5EC] bg-[#E5E5EC]' onPress={deleteUser}>
+                <Text className='text-[#505050]'>회원탈퇴</Text>
+              </Button>
+            </View>
+          </View>
+        </Wrap>
+      </Container>
+    </>
+  );
+}
